9d50a25dc693d7923fff852f62c03cf8cd9b2de5
Hamaad Siddiqui <hamaad_siddiqui@hotmail.com>
2022-07-24 13:50:38 +0500

    Google Login

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
+import { useSession } from 'next-auth/react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
-
 export default function Home() {
- return (
-  <>
-   <Head>
-    <title>Admin Panel</title>
-   </Head>
-   <Navbar />;
-  </>
- );
+ const { data: session } = useSession();
+
+ if (session) {
+  return (
+   <>
+    <Head>
+     <title>Admin Panel</title>
+    </Head>
+    <Navbar session={session} />;
+   </>
+  );
+ } else {
+  const router = useRouter();
+  useEffect(() => {
+   router.push('/api/auth/signin');
+  }, []);
+  return (
+   <>
+    <Head>
+     <title>Login</title>
+    </Head>
+    <div className='mt-40 text-center'>Loading...</div>
+   </>
+  );
+ }
 }
