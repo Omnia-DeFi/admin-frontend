a6b2ef4070dd347a5e8e77cca591e91bf774324f
Théo RICHARD <mail@rcode.tech>
2022-07-27 13:51:40 +0100

    refactor: add session components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,10 @@
-import { signIn, signOut, useSession } from "next-auth/client";
+import Session from "../containers/Session";
 import Link from "next/link";
+
 export default function Home() {
-  const [session, loading] = useSession();
-  console.log(session);
   return (
     <div>
-      {loading && <p>Loading..</p>}
-      {!session && (
-        <>
-          Not signed in <br />
-          <button
-            onClick={() =>
-              signIn("google", {
-                callbackUrl: "http://localhost:3000/dashboard",
-              })
-            }
-          >
-            Sign in
-          </button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session.user.name} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )}
+      <Session />
       <Link href="/dashboard">
         <a>Go to dashboard</a>
       </Link>
