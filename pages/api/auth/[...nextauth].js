fb4e0e0eaa3c755aa74470f6a0e541982acc3f06
Théo RICHARD <mail@rcode.tech>
2022-07-28 06:35:18 +0100

    feat(auth): only allow omniadefi.com addresses to login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,22 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        return profile.email_verified && profile.email.endsWith("@omniadefi.com");
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+    async session({ session, token, user }) {
+      if (token) {
+        session.id = token.id;
+      }
+      return session;
+    },
+  },
+
   theme: {
     colorScheme: "light",
   },
