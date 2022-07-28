4ebc47f559b2f87755152a575d5223dc1bbe62b1
Théo RICHARD <mail@rcode.tech>
2022-07-28 18:38:49 +0100

    feat: update var name in dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -6,7 +6,7 @@ import DeleteDataFrom from "../components/DeleteData";
 import UpdateData from "../components/UpdateData";
 import AddData from "../components/AddData";
 
-const Dashboard = ({ users, collectionName }) => {
+const Dashboard = ({ data, collectionName }) => {
   return (
     <>
       <Head>
@@ -16,24 +16,24 @@ const Dashboard = ({ users, collectionName }) => {
 
       <h1 className="text-center font-bold text-2xl mt-4">User</h1>
       <div className="w-auto min-w-[25%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch">
-        <AddData collection={collectionName} data={users} />
+        <AddData collection={collectionName} data={data} />
       </div>
       <div className="w-auto min-w-[33%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch">
         <ul>
-          {users.map((user) => (
-            <li key={user.id} className="border-b border-gray-600 p-2">
+          {data.map((item) => (
+            <li key={item.id} className="border-b border-gray-600 p-2">
               <div className="flex justify-between">
                 <div className="flex-1">
                   <p className="font-bold">Issuer</p>
                   <p className="text-md">Email</p>
                 </div>
                 <div className="flex-1">
-                  <h3 className="font-bold">{user.issuer}</h3>
-                  <p className="text-sm">{user.email}</p>
+                  <h3 className="font-bold">{item.issuer}</h3>
+                  <p className="text-sm">{item.email}</p>
                 </div>
                 <div className="flex-2">
-                  <UpdateData collection={"user"} data={user} />
-                  <DeleteDataFrom collection={"user"} data={user} />
+                  <UpdateData collection={"item"} data={item} />
+                  <DeleteDataFrom collection={"item"} data={item} />
                 </div>
               </div>
             </li>
@@ -47,7 +47,7 @@ const Dashboard = ({ users, collectionName }) => {
 export default Dashboard;
 
 export const getServerSideProps = async () => {
-  const users = await prisma.user.findMany({
+  const data = await prisma.user.findMany({
     select: {
       issuer: true,
       id: true,
@@ -57,7 +57,7 @@ export const getServerSideProps = async () => {
 
   return {
     props: {
-      users,
+      data,
       collectionName: "user",
     },
   };
