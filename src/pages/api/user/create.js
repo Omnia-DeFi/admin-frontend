1dc0b178531dedcbc99c3847178b78404e7a92a2
techx1 <ss.uitech01@gmail.com>
2022-08-05 15:33:15 +0500

    feat(create-user): update createUser api and added cors

--- src/pages/api/user/create.js
+++ src/pages/api/user/create.js
@@ -1,17 +1,39 @@
 import { prisma } from "../../../prisma/prisma";
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   const { issuer, email, phoneNumber, publicAddress } = req.body;
 
   try {
     const createdUser = await prisma.user
-      .create({
-        data: {
-          issuer,
-          email,
-          phone_number: +phoneNumber,
-          public_address: publicAddress,
+      .upsert({
+        where: {
+          email
         },
+        update: {},
+        create: {
+            issuer,
+            email,
+            phone_number: +phoneNumber,
+            public_address: publicAddress,
+        }
       })
       .catch(console.error)
       .finally(() => prisma.$disconnect());
