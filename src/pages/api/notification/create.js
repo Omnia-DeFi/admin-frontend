73a8f61ee42ae171abb9a97ceea68d52d6945f13
shamimbinnnur <sbnsbn00@gmail.com>
2022-08-07 22:27:55 +0600

    feature: notifications creating

---
+++ src/pages/api/notification/create.js
@@ -0,0 +1,53 @@
+import { prisma } from "../../../prisma/prisma";
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST'],
+  //todos: remove POST
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+  console.log("Boom")
+  const { userId } = req.body;
+
+  try {
+    //Intialize NotificationBearer
+    const notificationsBearer = await prisma.notificationsBearer.upsert({
+      where: {
+        userId: userId
+      },
+      update: {},
+      create: {
+        userId: userId,
+        bearerId: userId
+      },
+
+    })
+
+    //Create notification
+    const notifications = await prisma.notifications.create({
+      data: {
+        bearerId: userId
+      }
+    })
+    prisma.$disconnect();
+
+    res.status(200).json({message: "Notification Created", notificationsBearer, notifications  });
+  } catch (error) {
+    console.log(error);
+    //todo: Remove console
+    res.status(200).json({ error, message: "Notification Not Created"});
+  }
+}
