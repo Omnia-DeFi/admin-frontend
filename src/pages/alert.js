68225318fcdd137c2de0acfc970fcf8c968fe31b
techx1 <ss.uitech01@gmail.com>
2022-08-11 00:05:15 +0500

    feat(alert): add alert table with values

--- src/pages/alert.js
+++ src/pages/alert.js
@@ -2,50 +2,47 @@ import { prisma } from "../prisma/prisma";
 import Head from "next/head";
 
 import Navbar from "../components/Navbar";
-import Device from "../collections/Device/Device";
+import Alert from "../collections/Alert/Alert";
 
-const DevicePage = ({ data, collectionName }) => {
-  return (
+const AlertPage = ({ data, collectionName }) => {
+  return ( 
     <>
       <Head>
         <title>Admin Panel</title>
       </Head>
       <Navbar />
       <div className="mt-[40px]">
-        <Device collectionName={collectionName} data={data} />
+        <Alert collectionName={collectionName} data={data} />
       </div>
     </>
   );
 };
 
-export default DevicePage;
+export default AlertPage;
 
 export const getServerSideProps = async () => {
-  const data = await prisma.device.findMany({
+  const data = await prisma.alert.findMany({
     select: {
       id: true,
-      token: true,
-      user: {
-        select: {
-          email: true,
-          issuer: true,
-        },
-      },
-      reciever: {
-        select: {
-          title: true,
-          content: true,
-          type: true,
-          read: true,
-        },
-      },
+      content: true,
+      type: true,
+      // date: true,
+      read: true,
+      // reciever: {
+      //   select: {
+      //     title: true,
+      //     content: true,
+      //     type: true,
+      //     read: true,
+      //   },
+      // },
     },
   });
 
   return {
     props: {
       data,
-      collectionName: "device",
+      collectionName: "alert",
     },
   };
 };
