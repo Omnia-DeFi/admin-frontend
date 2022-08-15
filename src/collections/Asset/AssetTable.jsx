a043fd5d2f0972e2e0431c2d4a4b0fb0325b3290
techx1 <ss.uitech01@gmail.com>
2022-08-15 08:44:30 +0500

    fix: remove div from table and fix keys

--- src/collections/Asset/AssetTable.jsx
+++ src/collections/Asset/AssetTable.jsx
@@ -37,8 +37,7 @@ const AssetTable = ({ data }) => {
         </thead>
         <tbody>
           {data.map((item) => (
-            <div key={item.id}>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td
                   scope="row"
                   className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -60,7 +59,6 @@ const AssetTable = ({ data }) => {
                   <DeleteDataFrom collection={"asset"} data={item} />
                 </td>
               </tr>
-            </div>
           ))}
         </tbody>
       </table>
