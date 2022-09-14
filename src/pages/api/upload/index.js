37bbf8cd0cb7dc9aecfacbe5a4bb9ddb70cfad3c
Théo RICHARD <theo.richard@omniadefi.com>
2022-09-14 11:41:58 +0200

    chore: upload preset as env var

--- src/pages/api/upload/index.js
+++ src/pages/api/upload/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import { prisma } from "../../../prisma/prisma";
 
 const {cloudinary} = require("../../../utils/cloudinary");
@@ -10,7 +11,7 @@ export default async function handler(req, res) {
   try{
     const fileString = req.body.data;
     const uploadedResponse = await cloudinary.uploader.upload(fileString, {
-      upload_preset: 'omniadefi',
+      upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
       resource_type: 'auto'
     });
     res.status(200).json({ message: "File Uploaded", uploadedResponse });
