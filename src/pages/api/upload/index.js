5b9614417463993031ed3b6ee9274f219d561931
Théo RICHARD <theo.richard@omniadefi.com>
2022-09-14 11:41:58 +0200

    chore: upload preset as env var

--- src/pages/api/upload/index.js
+++ src/pages/api/upload/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { cloudinary } = require("../../../utils/cloudinary");
 
 export const config = {
@@ -8,8 +9,8 @@ export default async function handler(req, res) {
   try {
     const fileString = req.body.data;
     const uploadedResponse = await cloudinary.uploader.upload(fileString, {
-      upload_preset: "omniadefi",
-      resource_type: "auto",
+      upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
+      resource_type: 'auto'
     });
     res.status(200).json({ message: "File Uploaded", uploadedResponse });
   } catch (error) {
