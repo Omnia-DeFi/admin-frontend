ce2ed17b68822a1545d12dfad3ecee70a202e9bc
Anchal Gupta <anchal.gupta@w3villa.com>
2022-10-07 15:28:59 +0530

    feat: hello-world-pusher-basic-working

--- src/pages/api/notification/create.js
+++ src/pages/api/notification/create.js
@@ -1,5 +1,6 @@
 import { prisma } from "../../../prisma/prisma";
 import Cors from "cors";
+import Pusher from "pusher-js";
 
 const cors = Cors({
   methods: ["GET", "HEAD", "POST"],
@@ -18,6 +19,21 @@ function runMiddleware(req, res, fn) {
 }
 
 export default async function handler(req, res) {
+
+  const Pusher = require("pusher");
+
+    const pusher = new Pusher({
+      appId: "1483322",
+      key: "b2c6e10ed473266b458b",
+      secret: "5f842a05580230ae1197",
+      cluster: "eu",
+      useTLS: true
+    });
+  
+    pusher.trigger("omnia", "new-notification", {
+      message: "hello world from omnia"
+    });
+
   await runMiddleware(req, res, cors);
   const { userId, type, title, content } = req.body;
 
