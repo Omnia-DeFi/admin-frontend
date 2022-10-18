8d97a80c292151548d813b39c148e631ad1dea57
Anchal Gupta <anchal.gupta@w3villa.com>
2022-10-18 20:54:34 +0530

    fix: logout button fails to redirect to sign in button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
-import Router from "next/router";
+import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { GoogleButton } from "~/components";
 
 export default function Home() {
-  const { status } = useSession();
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  console.log("router is", router);
 
   if (status === "authenticated") {
-    return Router.push("/dashboard/users");
+    router.replace("/dashboard/users");
+    // window.location =  "http://localhost:3000/dashboard/hello";
   } else if (status === "unauthenticated") {
+    console.log("status is:", status);
     return (
       <>
         <Head>
